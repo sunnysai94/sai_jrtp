967940a9811ffcb27b17f418ba1bfbe34709b77a
Sai <sunnysaiprasad102@gmail.com>
2021-07-24 17:15:08 +0530

    book store commit

---
+++ src/main/java/sai_store/Book_Store.java
@@ -0,0 +1,13 @@
+package sai_store;
+
+public class Book_Store {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("welcome to my store");
+	
+	
+	}
+
+}
